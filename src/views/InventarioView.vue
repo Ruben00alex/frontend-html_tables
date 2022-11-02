edb6202de8e74074d98e140177cfa6ae79571893
Ruben00alex <ruben.00alex@gmail.com>
2022-11-01 18:27:51 -0700

    added inventarioView, not showing anything

---
+++ src/views/InventarioView.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable prettier/prettier -->
+
+<template>
+    <div class="inventarioTable">
+        <h1><b> Inventario </b></h1>
+        <p id="tablaInventario">
+        </p>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import IT from "@/components/InventarioTable.vue";
+
+export default {
+    name: "InventarioView",
+    components: {
+        IT,
+    },
+};
+</script>
