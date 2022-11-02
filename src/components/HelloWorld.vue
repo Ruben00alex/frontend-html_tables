e90e5d59f028769205285b0642cd12c4bb09f0bf
Ruben00alex <ruben.00alex@gmail.com>
2022-11-01 18:28:11 -0700

    Productos table is done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,97 +1,81 @@
+<!-- eslint-disable prettier/prettier -->
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
+  <div class="col-md-16">
+    <h1><b> Productos </b></h1>
+    <p id="tablaProductos">
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
   </div>
 </template>
 
-<script>
+<script lang="js">
+import axios from "axios";
 export default {
+
   name: "HelloWorld",
   props: {
     msg: String,
   },
+  created() {
+    axios.get("http://localhost:8080/api/productos").then((result) => {
+      let datosProductos = result.data;
+      //Aqui se crea la tabla
+      let tablaProductos = document.getElementById("tablaProductos");
+      let tabla = document.createElement("table");
+      let thead = document.createElement("thead");
+      let tr = document.createElement("tr");
+      let thId = document.createElement("th");
+      let thSku = document.createElement("th");
+      let thDescripcion = document.createElement("th");
+      let thUnidad = document.createElement("th");
+      let thCosto = document.createElement("th");
+      let thPrecio = document.createElement("th");
+
+      thId.innerHTML = "ID";
+      thSku.innerHTML = "SKU";
+      thDescripcion.innerHTML = "Descripcion";
+      thUnidad.innerHTML = "Unidad";
+      thCosto.innerHTML = "Costo";
+      thPrecio.innerHTML = "Precio";
+
+      tr.appendChild(thId);
+      tr.appendChild(thSku);
+      tr.appendChild(thDescripcion);
+      tr.appendChild(thUnidad);
+      tr.appendChild(thCosto);
+      tr.appendChild(thPrecio);
+      thead.appendChild(tr);
+      tabla.appendChild(thead);
+      tablaProductos.appendChild(tabla);
+      //add data from datosProductos to table
+      for (let i = 0; i < datosProductos.length; i++) {
+        let tr = document.createElement("tr");
+        let tdId = document.createElement("td");
+        let tdSku = document.createElement("td");
+        let tdDescripcion = document.createElement("td");
+        let tdUnidad = document.createElement("td");
+        let tdCosto = document.createElement("td");
+        let tdPrecio = document.createElement("td");
+
+        tdId.innerHTML = datosProductos[i].id;
+        tdSku.innerHTML = datosProductos[i].sku;
+        tdDescripcion.innerHTML = datosProductos[i].descripcion;
+        tdUnidad.innerHTML = datosProductos[i].unidad;
+        tdCosto.innerHTML = datosProductos[i].costo;
+        tdPrecio.innerHTML = datosProductos[i].precio;
+
+        tr.appendChild(tdId);
+        tr.appendChild(tdSku);
+        tr.appendChild(tdDescripcion);
+        tr.appendChild(tdUnidad);
+        tr.appendChild(tdCosto);
+        tr.appendChild(tdPrecio);
+        tabla.appendChild(tr);
+      }
+    }
+
+    )
+
+  }
 };
 </script>
 
@@ -100,14 +84,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
