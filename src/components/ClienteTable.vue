e0e9e6ed135154aeaf7c1e07200aeb4f110a9d63
Ruben00alex <ruben.00alex@gmail.com>
2022-11-02 23:22:43 -0700

    Created the Cliente Table component and View

---
+++ src/components/ClienteTable.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <p id="tablaCliente">
+        </p>
+    </div>
+
+</template>
+
+
+<script>
+const axios = require('axios');
+export default {
+    created() {
+        console.log("ClienteTable created");
+        axios.get("http://localhost:8080/api/cliente").then((result) => {
+            let datosProductos = result.data;
+            let tablaProductos = document.getElementById("tablaCliente");
+            let tabla = document.createElement("table");
+            let thead = document.createElement("thead");
+            let tr = document.createElement("tr");
+            let thRfc = document.createElement("th");
+            let thNombre = document.createElement("th");
+            let thEmail = document.createElement("th");
+
+            thRfc.innerHTML = "RFC";
+            thNombre.innerHTML = "Nombre";
+            thEmail.innerHTML = "Email";
+
+            tr.appendChild(thRfc);
+            tr.appendChild(thNombre);
+            tr.appendChild(thEmail);
+
+
+            thead.appendChild(tr);
+            tabla.appendChild(thead);
+            tablaProductos.appendChild(tabla);
+            for (let i = 0; i < datosProductos.length; i++) {
+                let tr = document.createElement("tr");
+                let tdRfc = document.createElement("td");
+                let tdNombre = document.createElement("td");
+                let tdEmail = document.createElement("td");
+
+                tdRfc.innerHTML = datosProductos[i].rfc;
+                tdNombre.innerHTML = datosProductos[i].nombre;
+                tdEmail.innerHTML = datosProductos[i].email;
+
+                tr.appendChild(tdRfc);
+                tr.appendChild(tdNombre);
+                tr.appendChild(tdEmail);
+
+                tabla.appendChild(tr);
+            }
+        }
+
+        )
+
+    }
+};
+</script>
---
+++ src/views/ClienteView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="centered">
+        <h1><b> Clientes </b></h1>
+        <ClienteTable />
+    </div>
+</template>
+
+
+<script>
+import ClienteTable from "../components/ClienteTable.vue";
+
+export default {
+    name: 'ClienteView',
+    components: {
+        ClienteTable,
+    },
+};
+
+</script>
+
+
+
+<style scoped>
+
+</style>
