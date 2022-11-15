5d6f893e28e3251846c0fd45aa8d68620aff25c5
Ruben00alex <ruben.00alex@gmail.com>
2022-11-15 13:58:24 -0800

    Changed styiling of nav/etc

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <title>Sitio e-commerce </title>
   <div class="navBar">
     <nav>
       <router-link to="/">Inicio</router-link>
@@ -6,46 +7,41 @@
       <router-link to="/inventario">Inventario</router-link>
       <router-link to="/clientes">Clientes</router-link>
     </nav>
+    <n-button id="botonLogin" type="primary" @click="signin">Iniciar sesión</n-button>
   </div>
 
-  <div>
-    <router-view />
-  </div>
 
+
+  <router-view />
 </template>
 
-<style>
-.inputProduct {
-  /* grid with 2 columns */
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  column-gap: 1rem;
-  row-gap: 1rem;
-  padding: 1rem;
 
-  align-items: center;
+<script>
+import { NButton } from 'naive-ui'
 
-  justify-items: center;
 
-  align-content: center;
-
-  justify-content: center;
-
-  background-color: #f1f1f1;
-
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.15);
-  margin-bottom: 1rem;
-  padding: 1.3rem;
-  border-radius: 0.5rem;
+export default {
+  name: 'App',
+  components: {
+    NButton
+  }
 }
 
+
+</script>
+
+
+<style>
 .navBar {
   position: relative;
-
-  overflow: hidden;
   width: auto;
   margin: 1rem 1rem;
+  /* Display elements next to each other */
+  display: flex;
+  /* Align items to the center */
+  /* Justify content to the center */
+  justify-content: center;
+
 }
 
 
@@ -59,13 +55,26 @@
 
 html {
   background-color: #21D4FD;
-  background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
-
-  background-size: cover;
-  /*fixed background*/
+  /*background-image: linear-gradient(19deg, #21D4FD, hsl(281, 100%, 46%), hsl(281, 100%, 56%), hsl(281, 100%, 86%), hsl(332, 100%, 56%), #21D4FD);*/
+  background-image: linear-gradient(to right, #fc5c7d, #6a82fb);
+  color: hsl(168, 100%, 45%);
+  background-size: 500%;
   background-attachment: fixed;
-  min-height: 100%;
+  /*fixed background*/
 
+  animation: bg_animation 25s ease infinite alternate;
+}
+
+@keyframes bg_animation {
+  0% {
+    background-position: left;
+  }
+
+
+  100% {
+
+    background-position: right;
+  }
 }
 
 body {
@@ -74,9 +83,8 @@ body {
 }
 
 h1 {
-  color: #9BD69F;
+  color: hsl(168, 100%, 45%);
   text-align: center;
-  text-shadow: 1px 1px 12px #2b3d2c;
 }
 
 #app {
@@ -87,32 +95,61 @@ h1 {
   color: #2c3e50;
 }
 
+#botonLogin {
+  position: relative;
+  right: 0;
+  margin: 1rem;
+}
+
 nav {
-  /*width is the amount of items*/
   width: fit-content;
-  padding: 15px;
-  /*make the width */
-  margin: 0 auto;
-  background-color: #009879;
+  padding: 0;
+
+  margin: 2rem auto;
+  background-color: hsl(168, 80%, 20%);
   border-radius: 10px;
-  box-shadow: 10px 10px 20px rgba(4, 105, 85, 0.532);
+  box-shadow: 3px 3px 20px rgba(4, 105, 85, 0.532);
 }
 
 nav a {
   font-weight: bold;
-  color: #fffbfb;
+  color: hsl(168, 100%, 75%);
   text-decoration: none;
-  padding: 10px;
-  border-radius: 10px;
-  margin: 0 10px;
-  background-color: #030098;
+  padding: 1rem;
+  margin: 0;
+  background-color: hsl(168, 80%, 25%);
+  transition: background-color 300ms ease-in-out, padding 300ms ease-in-out;
+
 
 
 }
 
+nav a:first-child {
+  margin-left: 0;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+
+}
+
+nav a:last-child {
+  margin-right: 0;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+
+nav a:focus,
+nav a:hover {
+  background-color: hsl(168, 100%, 30%);
+  color: hsl(168, 100%, 60%);
+  padding: 1.25rem 1.5rem;
+
+}
+
+
+
 
 nav a.router-link-exact-active {
 
-  color: #d2405d;
+  color: hsl(61, 100%, 60%);
 }
 </style>
