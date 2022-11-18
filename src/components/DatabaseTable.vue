8d0024be743ab9119cf6b3f7a80a0b30806e0e27
Ruben00alex <ruben.00alex@gmail.com>
2022-11-18 00:02:49 -0800

    changed to <script setup>

--- src/components/DatabaseTable.vue
+++ src/components/DatabaseTable.vue
@@ -5,111 +5,128 @@
 
 </template>
 
-<script lang="js">
+
+
+<script setup>
+import { ref, onMounted } from "vue";
 import axios from "axios";
 import { NSpin } from "naive-ui";
-export default {
+const loading = ref(true);
+let chosenTable = props.chosenTable;
 
-    data() {
-        return {
-            loading: true,
-        };
-    },
-
-    props: ["chosenTable"],
-
-    name: "DatabaseTable",
-    methods: {
-        updateTable() {
-            console.log("Updating table");
-            let url = "http://localhost:8080/api/";
-            url += this.chosenTable;
-            axios.get(url).then((result) => {
-                let datosProductos = result.data;
-
-                let tablaProductos = document.getElementById("tablaProductos"); //obtener la referencia a la tabla
-
-                //tabla es un elemento creado de tipo table dentro de tablaProductos
-                let tabla = tablaProductos.children[0];
-                //limpiar la tabla
-                tabla.innerHTML = "";
+//in defineExpose, we can define the methods that we want to expose to the parent component
+defineExpose({
+    loading,
+    chosenTable,
+    updateTable,
+});
 
 
-                let thead = document.createElement("thead");
-                let tr = document.createElement("tr");
-
-
-                //get datosProductos propsNames:
-                let propsNames = Object.getOwnPropertyNames(datosProductos[0]);
-                console.log(propsNames);
-
-                //crear table header
-                propsNames.forEach((propName) => {
-                    let th = document.createElement("th");
-                    th.innerHTML = propName;
-                    tr.appendChild(th);
-                });
-
-                thead.appendChild(tr);
-                tabla.appendChild(thead);
-
-
-                //add data from datosProductos to tabla
-                datosProductos.forEach((producto) => {
-                    let row = tabla.insertRow();
-
-                    for (let i = 0; i < propsNames.length; i++) {
-                        let cell = row.insertCell();
-                        cell.innerHTML = producto[propsNames[i]];
-                    }
-                });
-            });
-
-
-
-
-
-        }
-    },
-    created() {
-        let url = "http://localhost:8080/api/";
-        url += this.chosenTable;
-        axios.get(url).then((result) => {
-            let datosProductos = result.data;
-            //Aqui se crea la tabla
-            let tablaProductos = document.getElementById("tablaProductos");
-            let tabla = document.createElement("table");
-            let thead = document.createElement("thead");
-            let tr = document.createElement("tr");
-
-
-            let propsNames = Object.getOwnPropertyNames(datosProductos[0]);
-            console.log(propsNames);
-
-            //create table header
-            propsNames.forEach((propName) => {
-                let th = document.createElement("th");
-                th.innerHTML = propName;
-                tr.appendChild(th);
-            });
-
-            thead.appendChild(tr);
-            tabla.appendChild(thead);
-            tablaProductos.appendChild(tabla);
-            //add data from datosProductos to table
-            this.updateTable();
-        }
-
-        )
-
+const props = defineProps({
+    chosenTable: {
+        type: String,
+        required: true,
     }
-};
+});//props is an object that contains all the props that we defined in defineProps
 
-components: {
-    NSpin
+onMounted(() => {
+    loading.value = false;
+    console.log(chosenTable);
+
+    createTable();
+    console.log("Table created");
+});//onMounted is a lifecycle hook that is called when the component is mounted
+
+//Funcion para actualizar la tabla
+function updateTable() {
+    console.log("Updating table");
+    let url = "https://localhost:8080/api/";
+    url += chosenTable;
+    axios.get(url).then((result) => {
+        let datosProductos = result.data;
+
+        let tablaProductos = document.getElementById("tablaProductos"); //obtener la referencia a la tabla
+
+        //tabla es un elemento creado de tipo table dentro de tablaProductos
+        let tabla = tablaProductos.children[0];
+        //limpiar la tabla
+        tabla.innerHTML = "";
+
+
+        let thead = document.createElement("thead");
+        let tr = document.createElement("tr");
+
+
+        //get datosProductos propsNames:
+        let propsNames = Object.getOwnPropertyNames(datosProductos[0]);
+        console.log(propsNames);
+
+        //crear table header
+        propsNames.forEach((propName) => {
+            let th = document.createElement("th");
+            th.innerHTML = propName;
+            tr.appendChild(th);
+        });
+
+        thead.appendChild(tr);
+        tabla.appendChild(thead);
+
+
+        //add data from datosProductos to tabla
+        datosProductos.forEach((producto) => {
+            let row = tabla.insertRow();
+
+            for (let i = 0; i < propsNames.length; i++) {
+                let cell = row.insertCell();
+                cell.innerHTML = producto[propsNames[i]];
+            }
+        });
+    });
+
 }
 
+//Funcion para crear la tabla
+function createTable() {
+
+    let url = "https://localhost:8080/api/";
+    url += chosenTable;
+    axios.get(url).then((result) => {
+        let datosProductos = result.data;
+        //Aqui se crea la tabla
+        let tablaProductos = document.getElementById("tablaProductos");
+        let tabla = document.createElement("table");
+        let thead = document.createElement("thead");
+        let tr = document.createElement("tr");
+
+
+        let propsNames = Object.getOwnPropertyNames(datosProductos[0]);
+        console.log(propsNames);
+
+        //create table header
+        propsNames.forEach((propName) => {
+            let th = document.createElement("th");
+            th.innerHTML = propName;
+            tr.appendChild(th);
+        });
+
+        thead.appendChild(tr);
+        tabla.appendChild(thead);
+        tablaProductos.appendChild(tabla);
+        //add data from datosProductos to table
+        updateTable();
+    }
+
+    )
+
+
+}
+
+
+
+
 </script>
+
+
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style >
@@ -118,6 +135,7 @@ th,
 td {
     padding: 10px;
     text-align: center;
+
 }
 
 tr:last-of-type {
@@ -133,7 +151,7 @@ table {
     min-width: 400px;
     box-shadow: 10px 10px 20px hsla(257, 100%, 18%, 0.531);
 
-    width: fit-content + 10rem;
+    width: fit-content;
     font-family: 'Roboto', sans-serif;
 }
 
@@ -143,6 +161,16 @@ td {
 
     padding: 12px 15px;
     text-align: center;
+    max-width: 30ch;
+    overflow: hidden;
+    text-overflow: wrap;
+
+
+}
+
+td:first-child {
+    max-width: 25ch;
+
 }
 
 /* Zebra striping td */
